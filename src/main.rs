75f8aa60ae4c87afd7f92fea1027c135addf41d1
Rabian Ikwan <rabianikwan67@gmail.com>
2023-10-15 00:54:23 +0800

    feat: initial commit

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+mod database;
+mod config;
+
+use std;
+use actix_web::{App, get, HttpResponse, HttpServer, Responder};
+use crate::config::config::SETTING;
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("server is running healthy")
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let port = SETTING.server.port;
+    println!("service is running in http://127.0.0.1:{}", port);
+
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+    })
+        .bind(("127.0.0.1", 5000))?
+        .run()
+        .await
+}
